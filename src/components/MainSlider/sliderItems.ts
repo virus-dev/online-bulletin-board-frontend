6695092ebebddbcd0855ec1db90704751a87fa8a
Nikita <nikita-pashin12@yandex.ru>
2022-04-05 07:50:24 +0500

    add main slider v1

---
+++ src/components/MainSlider/sliderItems.ts
@@ -0,0 +1,45 @@
+export interface MainSliderItems {
+  imgUrl: string,
+  title: string,
+  subtitle: string,
+  href: string | null,
+}
+
+export default <MainSliderItems[]> [
+  {
+    imgUrl: 'https://cdn0.youla.io/files/images/720_720_out/60/36/6036265c29f3be1f1d409af2-1.jpg',
+    title: 'Оборот',
+    subtitle: 'Журнал для тех, кто на НеЮле',
+    href: null,
+  },
+  {
+    imgUrl: 'https://cdn0.youla.io/files/images/720_720_out/60/9a/609aac82dd24ce6ec717b042-1.jpg',
+    title: 'Отклюкайтесь',
+    subtitle: 'На вакансию',
+    href: null,
+  },
+  {
+    imgUrl: 'https://cache3.youla.io/files/images/720_720_out/5f/92/5f92e0f83935b97a64298b7b.jpg',
+    title: 'НеЮла стала безопаснее',
+    subtitle: 'Узнай больше',
+    href: null,
+  },
+  {
+    imgUrl: 'https://cdn0.youla.io/files/images/720_720_out/62/23/62235f373b419011564b9902-1.jpg',
+    title: 'Помогаем вместе',
+    subtitle: 'Фонды нуждаются в поддержке',
+    href: null,
+  },
+  {
+    imgUrl: 'https://cdn0.youla.io/files/images/720_720_out/62/1d/621ddfd0c3fe613fea564931-1.jpg',
+    title: 'Бытовая техника',
+    subtitle: 'По низким ценам',
+    href: null,
+  },
+  {
+    imgUrl: 'https://cdn0.youla.io/files/images/720_720_out/61/af/61af4b525c056a59422ec30c-1.jpg',
+    title: 'Попробуйте Тариф',
+    subtitle: 'Акция для бизнеса',
+    href: null,
+  },
+];
