367b41eb1df992126bfffaf41841ae149eb4fbb1
Nikita <nikita-pashin12@yandex.ru>
2022-05-13 18:27:05 +0500

    fix

--- src/utils/isProduction.ts
+++ src/utils/isProduction.ts
@@ -1,3 +1,3 @@
-const isProduction = (): boolean => process.env.NODE_ENV === 'production';
+const isProduction = (): boolean => process.env.NODE_ENV === 'production' || window.location.hostname === 'online-bulletin-board-frontend.herokuapp.com';
 
 export default isProduction;
